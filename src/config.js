fe78c9049b2e7170c36ee7dd8f714ddd9776c66c
haryati75 <haryati75@gmail.com>
2021-08-11 10:45:34 +0000

    change base_url for production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     'API_URL': "https://hh-tgc12p3-greattogo.herokuapp.com/api",
-    'BASE_URL': "https://3000-lavender-asp-vs9l1rsm.ws-us13.gitpod.io/",
+    'BASE_URL': "https://greattogo-foods.netlify.app/",
     'REFRESH_INTERVAL':  3600000, // 840000
     'EMAILJS_ID': 'user_Fvc9zxXoPQStYaW3e3DN0'
 }
